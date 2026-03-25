3cf098e902283ae58e4692a04dcb347fcb93e18e
Neha Saini <145911161+neha23nov@users.noreply.github.com>
2026-03-25 21:47:23 +0530

    add

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -33,12 +33,13 @@ async function registerUserController(req, res) {
     { expiresIn: "1d" }
   );
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "None",
-    path: "/"
-  });
+res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+  path: "/",
+  domain: "genai-uksj.onrender.com" 
+});
 
   res.status(201).json({
     message: "User registered successfully",
@@ -72,12 +73,13 @@ async function loginUserController(req, res) {
     { expiresIn: "1d" }
   );
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "None",
-    path: "/"
-  });
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+  path: "/",
+  domain: "genai-uksj.onrender.com" 
+});
 
   res.status(200).json({
     message: "User logged in successfully",
