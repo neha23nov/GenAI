3db9cc1380652c025c560f693c0943fdd0f08472
Neha Saini <145911161+neha23nov@users.noreply.github.com>
2026-03-25 19:23:11 +0530

    add

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const api=axios.create({
-    baseURL:'http://localhost:3000',
+    baseURL:'https://genai-uksj.onrender.com',
     withCredentials:true
 })
 
--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://genai-uksj.onrender.com",
     withCredentials: true,
 })
 
