2ee11eecb788ac24efc114b4d4d14a72bd037605
Neha Saini <145911161+neha23nov@users.noreply.github.com>
2026-03-25 19:39:48 +0530

    add

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,9 +7,9 @@ app.use(cookieParser())
 app.use(express.json())       
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+  origin: true,
+  credentials: true
+}));
 
 
 
